535d61a9276139eafad83a5b77a9994f58eca337
Ben <manglebot@users.noreply.github.com>
2024-02-08 14:50:51 +0000

    fix:remove console.log

--- src/components/Layouts/ScrollToTopOnMount.tsx
+++ src/components/Layouts/ScrollToTopOnMount.tsx
@@ -6,7 +6,6 @@ interface ScrollToTopOnMountProps {
 
 function ScrollToTopOnMount({ children }: ScrollToTopOnMountProps) {
   const handleScrollToTop = useCallback(() => {
-    console.log("ScrollToTopOnMount: useEffect triggered");
     window.scrollTo(0, 0);
   }, []);
 
