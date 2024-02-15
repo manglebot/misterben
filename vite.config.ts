bcc61331cd78cc0caf4540ac572c21e729763f86
Ben <manglebot@users.noreply.github.com>
2024-02-15 21:27:26 +0000

    fix:add code to fix github pages deploy issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/misterben/",
   plugins: [react()],
   build: {
-    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
   },
 });
