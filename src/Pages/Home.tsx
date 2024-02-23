7ae88fa6eaaed6f1c2a8a873d99b0655b9a05968
Ben <manglebot@users.noreply.github.com>
2024-02-23 11:03:51 +0000

    fix:update css on home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
       <div className="home-page__content">
         <p></p>
         <div className="home-page__collection">
-          <p className="home-page__content__link-1">
+          <p className="home-page__main-link-1">
             <h2>Work</h2>
             <NavLink onClick={closeMenu} to="/work">
               <img
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
               />
             </NavLink>
           </p>
-          <p className="home-page__content__link-2">
+          <p className="home-page__main-link-2">
             <h2>Play</h2>
             <NavLink onClick={closeMenu} to="/play">
               <img
@@ -34,7 +34,7 @@ const Home: React.FC = () => {
         </div>
 
         <h2>Hello</h2>
-        <div className="home-page__about-text">
+        <div className="home-page__about-section">
           <p className="home-page__about-text-paragraph">
             I'm Ben, a Manchester-based professional with over 15 years of
             experience in website development and graphic design. My expertise
@@ -50,26 +50,30 @@ const Home: React.FC = () => {
             boundaries.
           </p>
         </div>
-        <div className="social-media">
-          <a
-            className="social-media__link"
-            href="https://www.linkedin.com/in/ben-bayliss-uk/"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="400"
-              height="200"
-              fill="currentColor"
-              className="bi bi-linkedin"
-              viewBox="0 0 16 16"
+        <div className="home-page__about-section">
+          <p className="home-page__about-social-media">
+            <a
+              className="social-media__link"
+              href="https://www.linkedin.com/in/ben-bayliss-uk/"
             >
-              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-            </svg>
-            <br />
-            LinkedIn
-          </a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="400"
+                height="200"
+                fill="currentColor"
+                className="bi bi-linkedin"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+              </svg>
+              <br />
+              LinkedIn
+            </a>
+          </p>
 
-          <InstagramEmbed url="https://www.instagram.com/misterben/" />
+          <p className="home-page__about-social-media">
+            <InstagramEmbed url="https://www.instagram.com/misterben/" />
+          </p>
         </div>
         {/* eend about */}
 
