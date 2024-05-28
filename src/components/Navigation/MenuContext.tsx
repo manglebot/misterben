7d9fb71c079ab130399caef010f5ec22619fdbc8
Ben <manglebot@users.noreply.github.com>
2024-05-28 23:15:23 +0100

    feat:add back to top button code tweaks

--- src/components/Navigation/MenuContext.tsx
+++ src/components/Navigation/MenuContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-// import ScrollToTopOnMount from "../Layouts/ScrollToTopOnMount";
 
 interface MenuContextType {
   isMenuOpen: boolean;
