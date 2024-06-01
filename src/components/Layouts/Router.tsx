9b59608f7b2e4e543b9fd661207498c9c3472d83
Ben <manglebot@users.noreply.github.com>
2024-06-01 11:34:42 +0100

    cleanup: remove Play from router

--- src/components/Layouts/Router.tsx
+++ src/components/Layouts/Router.tsx
@@ -2,14 +2,11 @@ import { Route, Routes } from "react-router-dom";
 
 import MainLayout from "./MainLayout";
 import NotFound from "../../Pages/NotFound";
-// import Home from "../../Pages/Home";
 import Work from "../../Pages/Work";
-// import Play from "../../Pages/Play";
 import About from "../../Pages/About";
 import PageTemplate from "./PageTemplate";
 
 import workData from "../Data/workData";
-// import playData from "../Data/playData";
 import MenuProvider from "../Navigation/MenuContext";
 
 import ScrollToTop from "../Navigation/ScrollToTop";
@@ -22,7 +19,6 @@ const Router: React.FC = () => {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Work />} />
           <Route path="work" element={<Work />} />
-          {/* <Route path="play" element={<Play />} /> */}
           <Route path="about" element={<About />} />
 
           {workData.map((item, index) => (
@@ -34,10 +30,6 @@ const Router: React.FC = () => {
               }
             />
           ))}
-          {/* {playData.map((item, index) => (
-            <Route key={index} path={item.to} element={<item.element />} />
-          ))} */}
-
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
