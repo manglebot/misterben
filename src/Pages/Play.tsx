ff06c07673b54fdc48de405752d641952db240a8
Ben <manglebot@users.noreply.github.com>
2024-06-02 23:08:38 +0100

    fix:css layout tweaks

--- src/Pages/Play.tsx
+++ src/Pages/Play.tsx
@@ -1,18 +1,18 @@
-import PortfolioItem from "../components/Layouts/PortfolioItem";
-import "../components/Layouts/css/PortfolioItem.css";
-import playData from "../components/Data/playData";
+// import PortfolioItem from "../components/Layouts/PortfolioItem";
+// import "../components/Layouts/css/PortfolioItem.css";
+// import playData from "../components/Data/playData";
 
-const Play: React.FC = () => {
-  const portfolioItems = playData.map((item) => (
-    <PortfolioItem key={item.title} {...item} />
-  ));
+// const Play: React.FC = () => {
+//   const portfolioItems = playData.map((item) => (
+//     <PortfolioItem key={item.title} {...item} />
+//   ));
 
-  return (
-    <div>
-      <h1 className="main__title">PLAY</h1>
-      {portfolioItems}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1 className="main__title">PLAY</h1>
+//       {portfolioItems}
+//     </div>
+//   );
+// };
 
-export default Play;
+// export default Play;
