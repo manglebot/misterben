703fe49f21902dcc4df798efc39f9d167ab352b0
Ben <manglebot@users.noreply.github.com>
2024-06-02 23:07:40 +0100

    fix:close menu with gsap on click

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
+import { gsap, useGSAP } from "../../Utilities/gsapUtils";
 import Logo from "../Logo/Logo";
 import "./css/Navbar.css";
+import { useRef } from "react";
 
 const activeLink = "nav__link--active";
 const normalLink = "nav__link";
@@ -15,7 +17,29 @@ interface NavbarProps {
   setIsMenuOpen: (open: boolean) => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ isMenuOpen, setIsMenuOpen }) => {
+const Navbar: React.FC<NavbarProps> = ({ setIsMenuOpen }) => {
+  const navRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    console.log("navRef.current", navRef.current);
+    if (navRef.current) {
+      gsap.to(navRef.current, {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "15rem",
+        duration: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: navRef.current,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
+    }
+  }, []);
+
   const handleLinkClick = () => {
     setIsMenuOpen(false);
   };
